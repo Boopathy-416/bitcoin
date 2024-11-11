40e11a12510b6e91324126b6a6731a328f023fb9
Boopathy-416 <boopathy1865@gmail.com>
2024-11-11 14:57:41 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/register': {
+        target: 'http://ec2-54-84-228-107.compute-1.amazonaws.com:8080',
+        changeOrigin: true,
+        secure: false, // Set to false if your backend does not use HTTPS
+      },
+    },
+  },
+});
